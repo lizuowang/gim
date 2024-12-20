4d9462ee181463b4f35b1b0a88d932033bec56c7
lichunyu <504376304@qq.com>
2024-12-20 15:16:38 +0800

    服务注册-优化

--- pkg/etcd_registry/registry.go
+++ pkg/etcd_registry/registry.go
@@ -3,6 +3,7 @@ package etcd_registry
 import (
 	"context"
 	"log"
+	"time"
 
 	"github.com/lizuowang/gim/pkg/logger"
 	clientv3 "go.etcd.io/etcd/client/v3"
@@ -12,6 +13,8 @@ import (
 // ServiceRegister 创建租约注册服务
 type ServiceRegister struct {
 	cli     *clientv3.Client //etcd client
+	lease   int64            //租约时间
+	status  int              //状态 1 注册中 2 注册成功 3 关闭
 	leaseID clientv3.LeaseID //租约ID
 	//租约keepalieve相应chan
 	keepAliveChan <-chan *clientv3.LeaseKeepAliveResponse
@@ -23,9 +26,11 @@ type ServiceRegister struct {
 func NewServiceRegister(cli *clientv3.Client, name, addr string, lease int64) (*ServiceRegister, error) {
 
 	ser := &ServiceRegister{
-		cli:  cli,
-		name: name,
-		Addr: addr,
+		cli:    cli,
+		lease:  lease,
+		status: 1,
+		name:   name,
+		Addr:   addr,
 	}
 
 	//申请租约设置时间keepalive
@@ -39,12 +44,14 @@ func NewServiceRegister(cli *clientv3.Client, name, addr string, lease int64) (*
 // 设置租约
 func (s *ServiceRegister) putKeyWithLease(lease int64) error {
 	//设置租约时间
-	resp, err := s.cli.Grant(context.Background(), lease)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	resp, err := s.cli.Grant(ctx, lease)
 	if err != nil {
 		return err
 	}
 	//注册服务并绑定租约
-	_, err = s.cli.Put(context.Background(), s.name, s.Addr, clientv3.WithLease(resp.ID))
+	_, err = s.cli.Put(ctx, s.name, s.Addr, clientv3.WithLease(resp.ID))
 	if err != nil {
 		return err
 	}
@@ -56,6 +63,7 @@ func (s *ServiceRegister) putKeyWithLease(lease int64) error {
 	}
 	s.leaseID = resp.ID
 	s.keepAliveChan = leaseRespChan
+	s.status = 2
 	logger.L.Info("注册服务", zap.String("service_name", s.name), zap.String("addr", s.Addr))
 	return nil
 }
@@ -65,11 +73,31 @@ func (s *ServiceRegister) ListenLeaseRespChan(f func(leaseKeepResp *clientv3.Lea
 	for leaseKeepResp := range s.keepAliveChan {
 		f(leaseKeepResp)
 	}
+	logger.L.Info("关闭服务续租", zap.String("service_name", s.name))
+	// 如果是重试续租
+	for {
+		// 如果状态为关闭，则退出
+		if s.status == 3 {
+			break
+		}
+		err := s.putKeyWithLease(s.lease)
+		if err != nil {
+			logger.L.Error("etcd_registry 重试续租失败", zap.String("service_name", s.name), zap.Error(err))
+			time.Sleep(time.Second * 2)
+			continue
+		}
+		logger.L.Info("etcd_registry 重试续租成功", zap.String("service_name", s.name))
+		go s.ListenLeaseRespChan(f)
+		break
+	}
+
 	log.Println("关闭服务续租", s.name)
 }
 
 // Close 注销服务
 func (s *ServiceRegister) Close() error {
+	// 设置状态为关闭
+	s.status = 3
 	//撤销租约
 	if _, err := s.cli.Revoke(context.Background(), s.leaseID); err != nil {
 		return err
