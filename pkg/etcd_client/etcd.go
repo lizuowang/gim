716460460deeb1ed5f500f2bc1c3f94ea7bc8786
lichunyu <504376304@qq.com>
2024-12-20 11:14:53 +0800

    初始化etcd-检查服务是否可用

--- pkg/etcd_client/etcd.go
+++ pkg/etcd_client/etcd.go
@@ -1,8 +1,10 @@
 package etcd_client
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"time"
 
 	clientv3 "go.etcd.io/etcd/client/v3"
 )
@@ -16,6 +18,19 @@ func CreateClient(conf *clientv3.Config) (err error) {
 	if err != nil {
 		panic(fmt.Errorf("初始化etcd失败: %s %v", err, conf))
 	}
+
+	//检查etcd是否健康
+	// Try a simple operation to confirm the connection
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	for _, ep := range conf.Endpoints {
+		_, err := client.Status(ctx, ep)
+		if err != nil {
+			log.Printf("Failed to get status from %s: %v", ep, err)
+			panic(fmt.Errorf("初始化etcd失败: %s %v", ep, err))
+		}
+	}
+
 	log.Println("初始化etcd成功", err)
 	return
 }
